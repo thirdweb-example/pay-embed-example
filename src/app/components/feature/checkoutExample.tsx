edb7e417f62a8c2c7421c76eb672b129bc5ec5e6
Yash <67926590+Yash094@users.noreply.github.com>
2025-04-03 11:51:01 +0530

    fix build errors!

--- src/app/components/feature/checkoutExample.tsx
+++ src/app/components/feature/checkoutExample.tsx
@@ -1,7 +1,6 @@
-import { ThirdwebClient } from "thirdweb"
 import { PayEmbed, getDefaultToken } from "thirdweb/react";
 import { base } from "thirdweb/chains";
-import { client } from "../client"
+import { client } from "../../client"
 
 export default function CheckoutExample() {
     return (
